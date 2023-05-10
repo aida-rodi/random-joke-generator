191656d258a3b5dac9f2f61b64c7e7d0526e338f
aida-rodi <aida.rodi.1997@gmail.com>
2023-05-10 12:16:41 +0200

    Exercise 4

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,52 @@
+// Exercise 4
+window.addEventListener('load', async () => {
+  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&current_weather=true',);
+
+  const data = await response.json();
+  const currentWeather = data['current_weather'];
+
+  let weatherIcon = 'icon'
+
+  if (currentWeather.weathercode === 0) {
+    weatherIcon = '☀️'
+  } else if (currentWeather.weathercode === 1) {
+    weatherIcon = '🌤️'
+  } else if (currentWeather.weathercode === 2) {
+    weatherIcon = '⛅'
+  } else if (currentWeather.weathercode === 3) {
+    weatherIcon = '☁️'
+  } else if (currentWeather.weathercode === 45) {
+    weatherIcon = '🌫️'
+  } else if (currentWeather.weathercode === 48) {
+    weatherIcon = '🌫️'
+  } else if (currentWeather.weathercode === 51 || currentWeather.weathercode === 53 || currentWeather.weathercode === 55) {
+    weatherIcon = '🌦️'
+  } else if (currentWeather.weathercode === 61) {
+    weatherIcon = '🌧️'
+  } else if (currentWeather.weathercode === 63) {
+    weatherIcon = '🌧️🌧️'
+  } else if (currentWeather.weathercode === 65) {
+    weatherIcon = '🌧️🌧️🌧️'
+  } else if (currentWeather.weathercode === 71 || currentWeather.weathercode === 73 || currentWeather.weathercode === 75) {
+    weatherIcon = '🌨️'
+  } else if (currentWeather.weathercode === 95) {
+    weatherIcon = '⛈️'
+  } else {
+    weatherIcon = '⚠️'
+  };
+
+  console.log(`Temperature: ${currentWeather.temperature}°C`);
+  console.log(`Icon: ${weatherIcon}`);
+
+  const weatherParagraph = document.getElementById('weatherParagraph')
+  if (weatherParagraph) {
+    weatherParagraph.textContent = '';
+  }
+  weatherParagraph?.append(`${weatherIcon}  |  ${currentWeather.temperature}`);
+});
+
+// **************
+
 const ratedJokes: Array<object> = [];
 let ratedJoke: any = undefined;
 
